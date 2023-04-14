95611f46d17cc1279ea3c5ade78f749bb307426b
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-04-14 18:06:31 +0500

    Add Slider

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import NavBar from '../Components/NavBar/NavBar'
 import '../styles/Navbar/Navbar.css';
+import Slider from '../Components/Slider/Slider';
 const Hero = () => {
   return (
-    <> <NavBar /></>
+    <> <NavBar />
+    <Slider />
+    </>
    
   )
 }
