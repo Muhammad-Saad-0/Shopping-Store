e8b87fc3e7bccb32117b721327b584a484bb734b
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-04-20 18:14:43 +0500

    fetch single category products

--- src/Pages/SingleProduct.jsx
+++ src/Pages/SingleProduct.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { useParams } from 'react-router'
-const SingleProduct = () => {
-    const {categoryId}= useParams()
+import React, { useEffect, useState, useRef } from "react";
+import { AiFillStar } from "react-icons/ai";
+import "../styles/All Products/AllProducts.css";
+import { useParams } from "react-router";
+const SingleProducts = () => {
+    const {categoryId} = useParams()
+  const [productsData, setProductsData] = useState([]);
+
+  const fetchProducts = () => {
+    fetch(`https://dummyjson.com/products/category/${categoryId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProductsData(data.products);
+      });
+  };
+  useEffect(() => {
+    setTimeout(()=>{
+      fetchProducts();
+
+    },500)
+    console.log(productsData);
+  }, []);
   return (
- <>
- 
- </>
+    <section className="all-products-section">
+    <div>
+      <h3>{categoryId}</h3>
+    </div>
+    <span class="loader"></span>
+    <div className="products-grid">
+      {productsData.map(({ brand, id, images, title, price, rating }) => {
+          return (
+            <div key={id}>
+              <img src={images[3] ? images[3] : images[0]} alt={title} />
+              <span className="products-info-container">
+                <p>{title.length > 30 ?`${title.slice(0,30)}...`: title}</p>
+                <span className="products-detail">
+                  <p>${price}</p>
+                  {[1, 2, 3, 4, 5].map((r) => {
+                    return (
+                      <span className="ratings">
+                        {r <= Math.round(rating) ? (
+                          <AiFillStar style={{ color: "#FFDF00" }} />
+                        ) : (
+                          <AiFillStar style={{ color: "grey" }} />
+                        )}
+                      </span>
+                    );
+                  })}
+                  <p>{`(${Math.round(rating)})`}</p>
+                </span>
+              </span>
+            </div>
+          );
+        })}
+    </div>
+  </section>
   )
 }
 
-export default SingleProduct
+export default SingleProducts
