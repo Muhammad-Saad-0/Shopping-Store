a5db64489635347595e28ffa301ebf9b842de349
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-04-21 16:30:57 +0500

    add load more button

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -5,61 +5,73 @@ import "../styles/Products/Products.css";
 const AllProducts = () => {
   const [productsData, setProductsData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [limit, setLimit] = useState(25);
 
   const fetchProducts = () => {
-    fetch("https://dummyjson.com/products?limit=0")
+    fetch(`https://dummyjson.com/products?limit=${limit}`)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setProductsData(data.products);
-        setLoading(false)
+        setLoading(false);
       });
   };
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
   useEffect(() => {
-    setTimeout(() => {
-      fetchProducts();
-    }, 1000);
-    console.log(productsData);
-  }, []);
+    fetchProducts();
+  }, [limit]);
+  const loadMore = ()=>{
+    setLimit(prev => prev + 25)
+  }
   return (
     <>
-      {loading ? <span class="loader"></span> : false}
-
-      <section className="all-products-section">
-        <div>
-          <h3>Explore Our Products</h3>
-        </div>
-        <div className="products-grid">
-          {productsData.map(({ brand, id, images, title, price, rating }) => {
-            return (
-              <div key={id}>
-                <img src={images[3] ? images[3] : images[0]} alt={title} />
-                <span className="products-info-container">
-                  <p>
-                    {title.length > 30 ? `${title.slice(0, 30)}...` : title}
-                  </p>
-                  <span className="products-detail">
-                    <p>${price}</p>
-                    {[1, 2, 3, 4, 5].map((r) => {
-                      return (
-                        <span className="ratings">
-                          {r <= Math.round(rating) ? (
-                            <AiFillStar style={{ color: "#FFDF00" }} />
-                          ) : (
-                            <AiFillStar style={{ color: "grey" }} />
-                          )}
-                        </span>
-                      );
-                    })}
-                    <p>{`(${Math.round(rating)})`}</p>
+      {loading ? (
+        <span className="loader"></span>
+      ) : (
+        <section className="all-products-section">
+          <div>
+            <h3>Explore Our Products</h3>
+          </div>
+          <div className="products-grid">
+            {productsData.map(({ brand, id, images, title, price, rating }) => {
+              return (
+                <div key={id}>
+                  <img src={images[3] ? images[3] : images[0]} alt={title} />
+                  <span className="products-info-container">
+                    <p>
+                      {title.length > 30 ? `${title.slice(0, 30)}...` : title}
+                    </p>
+                    <span className="products-detail">
+                      <p>${price}</p>
+                      {[1, 2, 3, 4, 5].map((r) => {
+                        return (
+                          <span className="ratings">
+                            {r <= Math.round(rating) ? (
+                              <AiFillStar style={{ color: "#FFDF00" }} />
+                            ) : (
+                              <AiFillStar style={{ color: "grey" }} />
+                            )}
+                          </span>
+                        );
+                      })}
+                      <p>{`(${Math.round(rating)})`}</p>
+                    </span>
                   </span>
-                </span>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {limit < 100 &&   <button onClick={loadMore}>
+              Load More
+            </button> }
+          
+          </div>
+        </section>
+      )}
     </>
   );
 };
--- src/Pages/SingleCategory.jsx
+++ src/Pages/SingleCategory.jsx
@@ -3,7 +3,7 @@ import { AiFillStar } from "react-icons/ai";
 import "../styles/Products/Products.css";
 import { useParams } from "react-router";
 const SingleProducts = () => {
-    const {categoryId} = useParams()
+  const { categoryId } = useParams();
   const [productsData, setProductsData] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -13,65 +13,105 @@ const SingleProducts = () => {
         return response.json();
       })
       .then((data) => {
-        setProductsData(data.products);
-setLoading(false)
+        setLoading(false);
 
+        setProductsData(data.products);
       });
   };
   useEffect(() => {
-    setTimeout(()=>{
+    setTimeout(() => {
       fetchProducts();
-// setLoading(false)
-    },500)
+      // setLoading(false)
+    }, 500);
     console.log(productsData);
   }, []);
-//   useEffect(() => {
-//     setTimeout(()=>{
-// setLoading(false)
-//     },700)
-//   }, [productsData]);
+  //   useEffect(() => {
+  //     setTimeout(()=>{
+  // setLoading(false)
+  //     },700)
+  //   }, [productsData]);
   return (
     <>
-    {!loading &&
-    <div>
-    <span class="loader"></span> 
-    <section className="all-products-section">
-    <div>
-      <h3>{categoryId}</h3>
-    </div>
-       
-    <div className="products-grid">
-      {productsData.map(({ brand, id, images, title, price, rating }) => {
-          return (
-            <div key={id}>
-              <img src={images[3] ? images[3] : images[0]} alt={title} />
-              <span className="products-info-container">
-                <p>{title.length > 30 ?`${title.slice(0,30)}...`: title}</p>
-                <span className="products-detail">
-                  <p>${price}</p>
-                  {[1, 2, 3, 4, 5].map((r) => {
-                    return (
-                      <span className="ratings">
-                        {r <= Math.round(rating) ? (
-                          <AiFillStar style={{ color: "#FFDF00" }} />
-                        ) : (
-                          <AiFillStar style={{ color: "grey" }} />
-                        )}
-                      </span>
-                    );
-                  })}
-                  <p>{`(${Math.round(rating)})`}</p>
-                </span>
-              </span>
-            </div>
-          );
-        })}
-    </div>
-  </section>
-  </div>
-  } 
-  </>
-  )
-}
+      {loading ? (
+        <span className="loader"></span>
+      ) : (
+        <section className="all-products-section">
+          <div>
+            <h3>{categoryId}</h3>
+          </div>
 
-export default SingleProducts
+          <div className="products-grid">
+            {productsData.map(({ brand, id, images, title, price, rating }) => {
+              return (
+                <div key={id}>
+                  <img src={images[3] ? images[3] : images[0]} alt={title} />
+                  <span className="products-info-container">
+                    <p>
+                      {title.length > 30 ? `${title.slice(0, 30)}...` : title}
+                    </p>
+                    <span className="products-detail">
+                      <p>${price}</p>
+                      {[1, 2, 3, 4, 5].map((r) => {
+                        return (
+                          <span className="ratings">
+                            {r <= Math.round(rating) ? (
+                              <AiFillStar style={{ color: "#FFDF00" }} />
+                            ) : (
+                              <AiFillStar style={{ color: "grey" }} />
+                            )}
+                          </span>
+                        );
+                      })}
+                      <p>{`(${Math.round(rating)})`}</p>
+                    </span>
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      )}
+      {/* {loading && <span class="loader"></span>}
+
+      {!loading && (
+        <section className="all-products-section">
+          <div>
+            <h3>{categoryId}</h3>
+          </div>
+
+          <div className="products-grid">
+            {productsData.map(({ brand, id, images, title, price, rating }) => {
+              return (
+                <div key={id}>
+                  <img src={images[3] ? images[3] : images[0]} alt={title} />
+                  <span className="products-info-container">
+                    <p>
+                      {title.length > 30 ? `${title.slice(0, 30)}...` : title}
+                    </p>
+                    <span className="products-detail">
+                      <p>${price}</p>
+                      {[1, 2, 3, 4, 5].map((r) => {
+                        return (
+                          <span className="ratings">
+                            {r <= Math.round(rating) ? (
+                              <AiFillStar style={{ color: "#FFDF00" }} />
+                            ) : (
+                              <AiFillStar style={{ color: "grey" }} />
+                            )}
+                          </span>
+                        );
+                      })}
+                      <p>{`(${Math.round(rating)})`}</p>
+                    </span>
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      )} */}
+    </>
+  );
+};
+
+export default SingleProducts;
