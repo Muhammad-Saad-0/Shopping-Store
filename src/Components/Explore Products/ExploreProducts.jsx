bf3c39fee64fd94abc57138e128ff70fb3a9f9e0
Muhammad-Saad-0 <121944357+Muhammad-Saad-0@users.noreply.github.com>
2023-05-07 22:16:59 +0500

    make single product card

--- src/Components/Explore Products/ExploreProducts.jsx
+++ src/Components/Explore Products/ExploreProducts.jsx
@@ -17,6 +17,8 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db, auth } from "../../Data/firebase";
+import ProductCard from "../ProductsCard/ProductCard";
+
 const AllProducts = () => {
   const [productsData, setProductsData] = useState([]);
   const [inWishlist, setInWishlist] = useState(false);
@@ -35,81 +37,28 @@ const AllProducts = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
-  useEffect(() => {
-    auth.onAuthStateChanged(function (user) {
-      if (user) {
-        const checkWishlist = async () => {
-          let user = auth.currentUser;
-          const q = query(
-            collection(db, "WishList"),
-            where("uid", "==", user.uid)
-          );
-          const querySnapshot = await getDocs(q);
-          querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-          });
-          checkWishlist();
-        };
-      } else {
-        return false;
-      }
-    });
-  }, []);
-  const addToWishlist = async (id, images, title, price, rating) => {
-    await setDoc(doc(db, "WishList", `Product ${id}`), {
-      uid: auth.currentUser.uid,
-      productId: id,
-      images: images,
-      title: title,
-      price: price,
-      rating: rating,
-    });
-  };
-  const deleteWishlist = async (id)=>{
-    console.log(id);
-    const docRef = (db, "WishList", `Product ${id}`);
-console.log(docRef);
-    const docSnap = await getDoc(doc(db, "WishList", `Product ${id}`));
-    try {
-      if (docSnap.exists()) {
-        await deleteDoc(
-          (doc(db, "WishList", `Product ${id}`))
-          ,
-          where("uid", "==", auth.currentUser.uid)
-        );
-      }
-    } catch (error) {
-      console.log(error);
-    }
-   
-  }
-  //   useEffect(() => {
-  //     console.log(clickedIDs);
-  //   }, [clickedIDs]);
-  //   useEffect(() => {
-  //     const handleIcon = (id)=>{   if (clickedIDs.includes(id)) {
-  //       return (
-  //         <img
-  //           className="wishlist-icon"
-  //           src={deleteIcon}
-  //           alt=""
-  //         />
-  //       );
+  // useEffect(() => {
+  //   auth.onAuthStateChanged(function (user) {
+  //     if (user) {
+  //       const checkWishlist = async () => {
+  //         let user = auth.currentUser;
+  //         const q = query(
+  //           collection(db, "WishList"),
+  //           where("uid", "==", user.uid)
+  //         );
+  //         const querySnapshot = await getDocs(q);
+  //         querySnapshot.forEach((doc) => {
+  //           // doc.data() is never undefined for query doc snapshots
+  //           console.log(doc.id, " => ", doc.data());
+  //         });
+  //         checkWishlist();
+  //       };
+  //     } else {
+  //       return false;
   //     }
-  //     if (!clickedIDs.includes(id)) {
-  //       return (
-  //         <img className="wishlist-icon" src={heart} alt="" />
-  //       );
-  //     }
-  //     if ((clickedIDs.length = 0)) {
-  //       return (
-  //         <img className="wishlist-icon" src={heart} alt="" />
-  //       );
-  //     }}
-  //  handleIcon()
-  //   }, [clickedIDs]);
-  const idRef = useRef();
+  //   });
+  // }, []);
+  
   return (
     <>
       <section className="all-products-section">
@@ -121,68 +70,8 @@ console.log(docRef);
             .slice(0, 8)
             .map(({ id, images, title, price, rating }) => {
               return (
-                <Link id={id} key={id} to={`products/product/${id}`}>
-                  <div className="image-section">
-                    <img src={images[3] ? images[3] : images[0]} alt={title} />
-                    {inWishlist ? (
-                      <button
-                        id={id}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          e.preventDefault();
-                          setInWishlist(false)
-                          deleteWishlist(id);
-
-                        }}
-                      >
-                        <img
-                          className="wishlist-icon"
-                          src={deleteIcon}
-                          alt=""
-                        />
-                        {/* ) : (
-                        <img className="wishlist-icon" src={heart} alt="" />
-                      )} */}
-                      </button>
-                    ) : (
-                      <button
-                        id={id}
-                        onClick={(e) => {
-                          addToWishlist(id, images, title, price, rating);
-                          e.stopPropagation();
-                          e.preventDefault();
-                          setInWishlist(true)
-
-                        }}
-                      >
-                        <img className="wishlist-icon" src={heart} alt="" />
-                      </button>
-                    )}
-                  </div>
-
-                  <span className="products-info-container">
-                    <p>
-                      {title.length > 19 ? `${title.slice(0, 19)}...` : title}
-                    </p>
-                    <span className="products-detail">
-                      <p>${price}</p>
-                      <div className="ratings">
-                        {[1, 2, 3, 4, 5].map((r) => {
-                          return (
-                            <span key={uuidv4()}>
-                              {r <= Math.round(rating) ? (
-                                <AiFillStar style={{ color: "#FFDF00" }} />
-                              ) : (
-                                <AiFillStar style={{ color: "grey" }} />
-                              )}
-                            </span>
-                          );
-                        })}
-                        <p>{`(${Math.round(rating)})`}</p>
-                      </div>
-                    </span>
-                  </span>
-                </Link>
+               
+              <ProductCard id={id} images={images} title={title} price={price} rating={rating}/>
               );
               // return (
               //   <div key={id}>
